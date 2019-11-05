55637b459da6f2a1ad7eeb58f9ff58103b5f033c
Abdelrahman <AbdelrahmanH@hits-egypt.local>
2019-11-05 10:49:44 +0200

    Add project files.

---
+++ CollectionViewDemos/Views/Scrolling/IncrementalLoadingPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+using CollectionViewDemos.ViewModels;
+using Xamarin.Forms;
+
+namespace CollectionViewDemos.Views
+{
+    public partial class IncrementalLoadingPage : ContentPage
+    {
+        AnimalsViewModel animals = new AnimalsViewModel();
+        public ICommand NavigateCommand { get; private set; }
+        public IncrementalLoadingPage()
+        {
+            InitializeComponent();
+            //BindingContext = new AnimalsViewModel();
+            NavigateCommand = new Command<Type>(
+                async (Type pageType) =>
+                {
+                    Page page = (Page)Activator.CreateInstance(pageType);
+                    await Navigation.PushAsync(page);
+                });
+            BindingContext = this;
+        }
+
+        void OnCollectionViewRemainingItemsThresholdReached(object sender, EventArgs e)
+        {
+            // Retrieve more data here, or via the RemainingItemsThresholdReachedCommand.
+            // This sample retrieves more data using the RemainingItemsThresholdReachedCommand.
+        }
+
+    }
+}
